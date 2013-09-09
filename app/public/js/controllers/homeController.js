6c8236168b8dce67c18188873e5dee281b30637b
neobiwan <raymond.leung@me.com>
2013-09-09 11:57:47 +0800

    Introduction of Angular and migrate homeController and sign off to use angular

--- app/public/js/controllers/homeController.js
+++ app/public/js/controllers/homeController.js
@@ -1,53 +1,13 @@
 
-function HomeController()
-{
+'use strict';
 
-// bind event listeners to button clicks //
-	var that = this;
+angular.module('myApp').controller('HomeController', function($scope, $http) {
+// bind event listeners to $scope for ng-click //
 
 // handle user logout //
-	$('#btn-logout').click(function(){ that.attemptLogout(); });
 
-// confirm account deletion //
-	$('#account-form-btn1').click(function(){$('.modal-confirm').modal('show')});
 
-// handle account deletion //
-	$('.modal-confirm .submit').click(function(){ that.deleteAccount(); });
-
-	this.deleteAccount = function()
-	{
-		$('.modal-confirm').modal('hide');
-		var that = this;
-		$.ajax({
-			url: '/delete',
-			type: 'POST',
-			data: { id: $('#userId').val()},
-			success: function(data){
-	 			that.showLockedAlert('Your account has been deleted.<br>Redirecting you back to the homepage.');
-			},
-			error: function(jqXHR){
-				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
-			}
-		});
-	}
-
-	this.attemptLogout = function()
-	{
-		var that = this;
-		$.ajax({
-			url: '/home',
-			type: "POST",
-			data: {logout : true},
-			success: function(data){
-	 			that.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
-			},
-			error: function(jqXHR){
-				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
-			}
-		});
-	}
-
-	this.showLockedAlert = function(msg){
+	$scope.showLockedAlert = function(msg){
 		$('.modal-alert').modal({ show : false, keyboard : false, backdrop : 'static' });
 		$('.modal-alert .modal-header .modal-title').text('Success!');
 		$('.modal-alert .modal-body p').html(msg);
@@ -55,13 +15,32 @@ function HomeController()
 		$('.modal-alert button').click(function(){window.location.href = '/';})
 		setTimeout(function(){window.location.href = '/';}, 3000);
 	}
-}
+	
+	$scope.attemptLogout = function()
+	{
 
-HomeController.prototype.onUpdateSuccess = function()
-{
-	$('.modal-alert').modal({ show : false, keyboard : true, backdrop : true });
-	$('.modal-alert .modal-header .modal-title').text('Success!');
-	$('.modal-alert .modal-body p').html('Your account has been updated.');
-	$('.modal-alert').modal('show');
-	$('.modal-alert button').off('click');
-}
+		$http({method: 'POST', url: '/home', params: {"logout" : "true"}}).
+		success(function(data, status, headers, config) {
+			$scope.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
+		}).
+		error(function(data, status, headers, config) {
+			console.log('Error '+ status);
+		});
+		//var that = this;
+		/*$.ajax({
+			url: '/home',
+			type: "POST",
+			data: {logout : true},
+			success: function(data){
+	 			$scope.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
+			},
+			error: function(jqXHR){
+				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
+			}
+		});*/
+	}
+
+//}
+
+});
+
